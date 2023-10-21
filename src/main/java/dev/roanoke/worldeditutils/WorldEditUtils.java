19a4aeec12883f4b04ada71cebd76ae65388855c
Xander Forrest <xander@xanderforrest.com>
2023-10-21 17:10:03 +0100

    Init

---
+++ src/main/java/dev/roanoke/worldeditutils/WorldEditUtils.java
@@ -0,0 +1,13 @@
+package dev.roanoke.worldeditutils;
+
+import net.fabricmc.api.ModInitializer;
+
+public class WorldEditUtils implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
